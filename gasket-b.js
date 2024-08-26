66958ede5eb8bb0d6f2d82ba142ef4b8a1cf682c
Kjartan Oli Agustsson <koa20@hi.is>
2024-08-26 19:15:46 +0000

    Modularise 1.b

--- gasket-b.js
+++ gasket-b.js
@@ -1,45 +1,72 @@
 "use strict";
 
-var gl;
-var points;
+let gl = null;
+let program = null;
 
-var NumPoints = 5000;
+const num_points = 5000;
 
-function init_b()
+export function init()
 {
-	var canvas = document.getElementById( "1b" );
+	const canvas = document.querySelector("#d1b-canvas");
 
-	gl = WebGLUtils.setupWebGL( canvas );
+	gl = WebGLUtils.setupWebGL(canvas);
 	if ( !gl ) { alert( "WebGL isn't available" ); }
 
+	//
+	//	Configure WebGL
+	//
+	gl.viewport( 0, 0, canvas.width, canvas.height );
+	gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+	//	Load shaders and initialize attribute buffers
+
+	program = initShaders( gl, "vertex-shader", "fragment-shader" );
+	gl.useProgram(program);
+
+	setup_points(num_points);
+	render(num_points);
+};
+
+function setup_points(num_points) {
+	// Load the data into the GPU
+	const points = create_points(num_points);
+	const bufferId = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+	gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+	// Associate out shader variables with our data buffer
+	const vPosition = gl.getAttribLocation(program, "vPosition");
+	gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vPosition );
+}
+
+function create_points(num_points) {
 	//
 	//	Initialize our data for the Sierpinski Gasket
 	//
 
 	// First, initialize the corners of our gasket with three points.
 
-	var vertices = [
-		vec2( -1, -1 ),
-		vec2(	 0,	 1 ),
-		vec2(	 1, -1 )
+	const vertices = [
+		vec2(-1, -1),
+		vec2(0, 1),
+		vec2(1, -1),
 	];
 
 	// Specify a starting point p for our iterations
 	// p must lie inside any set of three vertices
 
-	var u = add( vertices[0], vertices[1] );
-	var v = add( vertices[0], vertices[2] );
-	var p = scale( 0.25, add( u, v ) );
+	let p = vec2(100, 100);
 
 	// And, add our initial point into our array of points
 
-	points = [ p ];
+	const points = [ p ];
 
 	// Compute new points
 	// Each new point is located midway between
 	// last point and a randomly chosen vertex
 
-	for ( var i = 0; points.length < NumPoints; ++i ) {
+	for ( var i = 0; points.length < num_points; ++i ) {
 		var j = ((r) => {
 			if (r < 0.9)
 				return 0;
@@ -53,34 +80,11 @@ function init_b()
 		points.push( p );
 	}
 
-	//
-	//	Configure WebGL
-	//
-	gl.viewport( 0, 0, canvas.width, canvas.height );
-	gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-
-	//	Load shaders and initialize attribute buffers
-
-	var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-	gl.useProgram( program );
-
-	// Load the data into the GPU
-
-	var bufferId = gl.createBuffer();
-	gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-	gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
-
-	// Associate out shader variables with our data buffer
-
-	var vPosition = gl.getAttribLocation( program, "vPosition" );
-	gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-	gl.enableVertexAttribArray( vPosition );
-
-	render();
-};
+	return points;
+}
 
 
-function render() {
-	gl.clear( gl.COLOR_BUFFER_BIT );
-	gl.drawArrays( gl.POINTS, 0, points.length );
+function render(num_points) {
+	gl.clear(gl.COLOR_BUFFER_BIT );
+	gl.drawArrays(gl.POINTS, 0, num_points);
 }
